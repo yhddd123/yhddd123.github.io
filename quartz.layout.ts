64c40643b56ddecc4420b31687cd4cb4b333283c
yhddd123 <gdfyhmyhm@gmail.com>
2025-09-09 22:06:06 +0800

    250909 try

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -118,10 +118,28 @@ export const defaultContentPageLayout: PageLayout = {
     }),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 0.8, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
+        fontSize: 0.6, // what size should the node labels be?
+        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+        enableRadial: false, // whether to constrain the graph, similar to Obsidian
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.RecentNotes({ limit: 5 }),
+    Component.RecentNotes({ 
+      limit: 3,
+      showTags: false,
+    }),
   ],
 }
 
