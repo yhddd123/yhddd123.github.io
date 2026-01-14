b96458720b7219ee390b7032e0279f9715e228e5
yhm <161533456+yhddd123@users.noreply.github.com>
2026-01-14 22:23:48 +0800

    Revert "recent more"
    This reverts commit 12863bfcf524eea6eb199126286c9877e1e2359f.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -61,7 +61,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.RecentNotes({ 
       limit: 2,
       showTags: false,
-      linkToMore: "recentnote",
+      linkToMore: "components",
     }),
   ],
   right: [
